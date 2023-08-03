884b8c2c6b3aa82286394a1371b0f74b847f5999
Sajalthehacker <97801174+Sajalthehacker@users.noreply.github.com>
2023-08-04 00:05:00 +0530

    Time: 0 ms (100%), Space: 6.5 MB (31.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,60 +1,38 @@
 class Solution {
 public:
-    void solve(unordered_map<char, vector<char>> mp, string digits, string cur, vector<string> &ans, int ind){
-        if(ind == digits.size()){
-            ans.push_back(cur);
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.empty()) return result;
+        
+        unordered_map<char, string> mapping = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+        
+        string currentCombination;
+        backtrack(digits, 0, mapping, currentCombination, result);
+        
+        return result;
+    }
+    
+    void backtrack(const string& digits, int index, const unordered_map<char, string>& mapping, string& currentCombination, vector<string>& result) {
+        if (index == digits.length()) {
+            result.push_back(currentCombination);
             return;
         }
-
-        for(int i = 0; i < mp[digits[ind]].size(); i++){
-            solve(mp, digits, cur + mp[digits[ind]][i], ans, ind + 1);
-        }
-
-    }
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits == ""){
-            return ans;
-        }
-        unordered_map<char, vector<char>> mp;
-        mp['2'].push_back('a');
-        mp['2'].push_back('b');
-        mp['2'].push_back('c');
-
-        mp['3'].push_back('d');
-        mp['3'].push_back('e');
-        mp['3'].push_back('f');
-
-        mp['4'].push_back('g');
-        mp['4'].push_back('h');
-        mp['4'].push_back('i');
         
-        mp['5'].push_back('j');
-        mp['5'].push_back('k');
-        mp['5'].push_back('l');
-
-        mp['6'].push_back('m');
-        mp['6'].push_back('n');
-        mp['6'].push_back('o');
-
-        mp['7'].push_back('p');
-        mp['7'].push_back('q');
-        mp['7'].push_back('r');
-        mp['7'].push_back('s');
-
-        mp['8'].push_back('t');
-        mp['8'].push_back('u');
-        mp['8'].push_back('v');
-
-        mp['9'].push_back('w');
-        mp['9'].push_back('x');
-        mp['9'].push_back('y');
-        mp['9'].push_back('z');
-
-        string cur = "";
-
-        solve(mp, digits, cur, ans, 0);
-
-        return ans;
+        char digit = digits[index];
+        string letters = mapping.at(digit);
+        for (char letter : letters) {
+            currentCombination.push_back(letter);
+            backtrack(digits, index + 1, mapping, currentCombination, result);
+            currentCombination.pop_back(); // Backtrack by removing the last letter added
+        }
     }
 };
