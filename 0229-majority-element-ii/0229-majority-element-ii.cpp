2fcdef96cddb4b62d41e767a248c6e58d9a29702
Sajalthehacker <97801174+Sajalthehacker@users.noreply.github.com>
2023-08-07 00:14:46 +0530

    Time: 14 ms (61.75%), Space: 15.9 MB (24.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,47 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
         vector<int> ans;
-        map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(auto it = mp.begin(); it != mp.end(); it++){
-            if(it->second > nums.size() / 3){
-                ans.push_back(it->first);
+        int can1 = INT_MAX, can2 = INT_MAX;
+        int c1 = 0, c2 = 0;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == can1){
+                c1++;
             }
+            else if(nums[i] == can2){
+                c2++;
+            }
+            else if(c1 == 0){
+                c1++;
+                can1 = nums[i];
+            }
+            else if(c2 == 0){
+                c2++;
+                can2 = nums[i];
+            }
+            else{
+                c1--;
+                c2--;
+            }
+        }
+
+        int freq1 = 0, freq2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == can1){
+                freq1++;
+            }
+            else if(nums[i] == can2){
+                freq2++;
+            }
+        }
+
+        if(freq1 > n / 3){
+            ans.push_back(can1);
+        }
+        if(freq2 > n / 3){
+            ans.push_back(can2);
         }
         return ans;
     }
